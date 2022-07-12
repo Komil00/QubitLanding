e321d50575af6122a06956ba5958253156792575
Komil00 <komiltuev@gmail.com>
2022-07-12 15:56:34 +0500

    work in admin view imagesrrr

--- app/viewset.py
+++ app/viewset.py
@@ -1,87 +1,79 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+
 from .models import Category, Project, ProjectImage, Contact, ArticleCategory, Article, Tag, Service
 from .serializers import *
 
 
-class CategoryView(viewsets.ModelViewSet):
+class CategoryView(viewsets.ViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ProjectView(viewsets.ModelViewSet):
+class ProjectView(viewsets.ViewSet):
     queryset = Project.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ProjectListSerializer
-        if self.action == "retrieve":
-            return ProjectListSerializer
-        else:
-            return ProjectPostSerializer
+    def list(self, request):
+        queryset = Project.objects.all()
+        serializer = ProjectListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ProjectImageView(viewsets.ModelViewSet):
+class ProjectImageView(viewsets.ViewSet):
     queryset = ProjectImage.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ProjectListImageSerializer
-        if self.action == "retrieve":
-            return ProjectListImageSerializer
-        else:
-            return ProjectPostImageSerializer
+    def list(self, request):
+        queryset = ProjectImage.objects.all()
+        serializer = ProjectListImageSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ContactView(viewsets.ModelViewSet):
+class ContactView(viewsets.ViewSet):
     queryset = Contact.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ContactListSerializer
-        if self.action == "retrieve":
-            return ContactListSerializer
-        else:
-            return ContactPostSerializer
+    def list(self, request):
+        queryset = Contact.objects.all()
+        serializer = ContactListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ArticleCategoryView(viewsets.ModelViewSet):
+class ArticleCategoryView(viewsets.ViewSet):
     queryset = ArticleCategory.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ArticleListCategorySerializer
-        if self.action == "retrieve":
-            return ArticleListCategorySerializer
-        else:
-            return ArticlePostCategorySerializer
+    def list(self, request):
+        queryset = ArticleCategory.objects.all()
+        serializer = ArticleListCategorySerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class TagView(viewsets.ModelViewSet):
+class TagView(viewsets.ViewSet):
     queryset = Tag.objects.all()
-    serializer_class = TagSerializer
+
+    def list(self, request):
+        queryset = Tag.objects.all()
+        serializer = TagSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ArticleView(viewsets.ModelViewSet):
+class ArticleView(viewsets.ViewSet):
     queryset = Article.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ArticleListSerializer
-        if self.action == "retrieve":
-            return ArticleListSerializer
-        else:
-            return ArticlePostSerializer
+    def list(self, request):
+        queryset = Article.objects.all()
+        serializer = ArticleListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-class ServiceView(viewsets.ModelViewSet):
+class ServiceView(viewsets.ViewSet):
     queryset = Service.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == "list":
-            return ServiceListSerializer
-        if self.action == "retrieve":
-            return ServiceListSerializer
-        else:
-            return ServicePostSerializer
+    def list(self, request):
+        queryset = Service.objects.all()
+        serializer = ServiceListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
