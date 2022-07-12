2811c5c1c6adacb31700d6a5e1650bf4bac352bd
Komil00 <komiltuev@gmail.com>
2022-07-12 16:20:49 +0500

    work in admin view imagesrrr

--- app/viewset.py
+++ app/viewset.py
@@ -1,79 +1,144 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
-
+from rest_framework import viewsets, permissions
 from .models import Category, Project, ProjectImage, Contact, ArticleCategory, Article, Tag, Service
-from .serializers import *
+from .serializers import CategorySerializer, ProjectListSerializer, ProjectPostSerializer, ProjectListImageSerializer, \
+    ProjectPostImageSerializer, ContactListSerializer, ContactPostSerializer, ArticleListCategorySerializer, \
+    ArticlePostCategorySerializer, TagSerializer, ArticleListSerializer, ArticlePostSerializer, ServiceListSerializer, \
+    ServicePostSerializer
 
 
-class CategoryView(viewsets.ViewSet):
+class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    def list(self, request):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ProjectView(viewsets.ViewSet):
+class ProjectView(viewsets.ModelViewSet):
     queryset = Project.objects.all()
 
-    def list(self, request):
-        queryset = Project.objects.all()
-        serializer = ProjectListSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProjectListSerializer
+        if self.action == "retrieve":
+            return ProjectListSerializer
+        else:
+            return ProjectPostSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ProjectImageView(viewsets.ViewSet):
+class ProjectImageView(viewsets.ModelViewSet):
     queryset = ProjectImage.objects.all()
 
-    def list(self, request):
-        queryset = ProjectImage.objects.all()
-        serializer = ProjectListImageSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProjectListImageSerializer
+        if self.action == "retrieve":
+            return ProjectListImageSerializer
+        else:
+            return ProjectPostImageSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ContactView(viewsets.ViewSet):
+class ContactView(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
 
-    def list(self, request):
-        queryset = Contact.objects.all()
-        serializer = ContactListSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ContactListSerializer
+        if self.action == "retrieve":
+            return ContactListSerializer
+        else:
+            return ContactPostSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ArticleCategoryView(viewsets.ViewSet):
+class ArticleCategoryView(viewsets.ModelViewSet):
     queryset = ArticleCategory.objects.all()
 
-    def list(self, request):
-        queryset = ArticleCategory.objects.all()
-        serializer = ArticleListCategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ArticleListCategorySerializer
+        if self.action == "retrieve":
+            return ArticleListCategorySerializer
+        else:
+            return ArticlePostCategorySerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class TagView(viewsets.ViewSet):
+class TagView(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
+    serializer_class = TagSerializer
 
-    def list(self, request):
-        queryset = Tag.objects.all()
-        serializer = TagSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ArticleView(viewsets.ViewSet):
+class ArticleView(viewsets.ModelViewSet):
     queryset = Article.objects.all()
 
-    def list(self, request):
-        queryset = Article.objects.all()
-        serializer = ArticleListSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ArticleListSerializer
+        if self.action == "retrieve":
+            return ArticleListSerializer
+        else:
+            return ArticlePostSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
 
-class ServiceView(viewsets.ViewSet):
+class ServiceView(viewsets.ModelViewSet):
     queryset = Service.objects.all()
 
-    def list(self, request):
-        queryset = Service.objects.all()
-        serializer = ServiceListSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super(self.__class__, self).get_permissions()
 
-
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ServiceListSerializer
+        if self.action == "retrieve":
+            return ServiceListSerializer
+        else:
+            return ServicePostSerializer
